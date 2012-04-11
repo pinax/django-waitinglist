939129e042767ac82600b7f9bc7e9f0531329e99
Brian Rosner <brosner@gmail.com>
2012-04-11 15:00:49 -0600

    initial commit

---
+++ README.rst
@@ -0,0 +1,16 @@
+===============================================
+django-waitinglist: waiting list app for Django
+===============================================
+
+Provides a waiting list and tools to manage it.
+
+Requirements
+============
+
+* Django 1.4
+* django-appconf (included in ``install_requires``)
+
+Documentation
+=============
+
+See http://django-waitinglist.readthedocs.org/
---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name = "django-waitinglist",
+    version = "1.0b1.dev1",
+    author = "Brian Rosner",
+    author_email = "brosner@gmail.com",
+    description = "a Django waiting list app for private betas",
+    long_description = open("README.rst").read(),
+    license = "MIT",
+    url = "http://github.com/pinax/django-waitinglist",
+    packages = find_packages(),
+    install_requires = [
+        "django-appconf==0.5",
+    ],
+    classifiers = [
+        "Development Status :: 4 - Beta",
+        "Environment :: Web Environment",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Framework :: Django",
+    ]
+)
