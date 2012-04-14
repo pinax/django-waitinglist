61e69d0abf24988d9a34dc59dabca1478a3ffdc1
Brian Rosner <brosner@gmail.com>
2012-04-14 13:55:18 -0600

    simplified {% waitinglist_entry_form %} with assignment_tag

--- waitinglist/templatetags/waitinglist_tags.py
+++ waitinglist/templatetags/waitinglist_tags.py
@@ -6,21 +6,8 @@ from waitinglist.forms import WaitingListEntryForm
 register = template.Library()
 
 
-class WaitingListEntryFormNode(template.Node):
-    
-    def __init__(self, as_varname):
-        self.as_varname = as_varname
-    
-    def get_form(self, context):
-        return WaitingListEntryForm()
-    
-    def render(self, context):
-        context[self.as_varname] = self.get_form(context)
-        return ""
-
-
-@register.tag
-def waitinglist_entry_form(parser, token):
+@register.assignment_tag
+def waitinglist_entry_form():
     """
     Get a (new) form object to post a new comment.
     
@@ -29,7 +16,4 @@ def waitinglist_entry_form(parser, token):
         {% waitinglist_entry_form as [varname] %}
     
     """
-    bits = token.split_contents()
-    if len(bits) != 3:
-        raise template.TemplateSyntaxError("'%s' takes one 'as' argument" % bits[0])
-    return WaitingListEntryFormNode(bits[2])
+    return WaitingListEntryForm()
