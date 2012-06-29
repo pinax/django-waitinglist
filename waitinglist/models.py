df136718fc80bba7837ca8bc71c29202b8924db7
Patrick Altman <paltman@gmail.com>
2012-06-29 16:18:51 -0500

    Fail silently if an invalid code is used

--- waitinglist/models.py
+++ waitinglist/models.py
@@ -89,6 +89,9 @@ def handle_user_signup(sender, **kwargs):
     signup_code = kwargs["form"].cleaned_data["code"]
     # fetch the cohort for the signup code
     qs = SignupCodeCohort.objects.select_related("cohort")
-    cohort = qs.get(signup_code__code=signup_code).cohort
-    # create a UserCohort for user association to a cohort
-    UserCohort.objects.create(user=kwargs["user"], cohort=cohort)
+    try:
+        cohort = qs.get(signup_code__code=signup_code).cohort
+        # create a UserCohort for user association to a cohort
+        UserCohort.objects.create(user=kwargs["user"], cohort=cohort)
+    except SignupCodeCohort.DoesNotExist:
+        pass
