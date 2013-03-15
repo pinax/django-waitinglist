7d1cc824bc160181078d868b72f6766a5c3b7004
Patrick Altman <paltman@gmail.com>
2013-03-14 19:01:58 -0500

    Make compatible with Django 1.5

--- waitinglist/urls.py
+++ waitinglist/urls.py
@@ -1,12 +1,12 @@
 from django.conf.urls.defaults import patterns, url
-from django.views.generic.simple import direct_to_template
+from django.views.generic import TemplateView
 
 
 urlpatterns = patterns("",
     url(r"^list_signup/$", "waitinglist.views.list_signup", name="waitinglist_list_signup"),
     url(r"^ajax_list_signup/$", "waitinglist.views.ajax_list_signup", name="waitinglist_ajax_list_signup"),
-    url(r"^success/$", direct_to_template, {"template": "waitinglist/success.html"}, name="waitinglist_success"),
     url(r"^survey/(?P<code>.*)/$", "waitinglist.views.survey", name="waitinglist_survey"),
+    url(r"^success/$", TemplateView.as_view(template_name="waitinglist/success.html"), name="waitinglist_success"),
     url(r"^cohorts/$", "waitinglist.views.cohort_list", name="waitinglist_cohort_list"),
     url(r"^cohorts/create/$", "waitinglist.views.cohort_create", name="waitinglist_cohort_create"),
     url(r"^cohorts/cohort/(\d+)/$", "waitinglist.views.cohort_detail", name="waitinglist_cohort_detail"),
