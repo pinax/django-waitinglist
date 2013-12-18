666440573aacd8155533d27cdbdce44995d00758
Thomas Schreiber <tom@rizu.fake>
2013-12-18 18:17:33 +0100

    lint

--- waitinglist/urls.py
+++ waitinglist/urls.py
@@ -2,7 +2,9 @@ from django.conf.urls import patterns, url
 from django.views.generic import TemplateView
 
 
-urlpatterns = patterns("",
+urlpatterns = patterns(
+    "",
+
     url(r"^list_signup/$", "waitinglist.views.list_signup", name="waitinglist_list_signup"),
     url(r"^ajax_list_signup/$", "waitinglist.views.ajax_list_signup", name="waitinglist_ajax_list_signup"),
     url(r"^survey/thanks/$", TemplateView.as_view(template_name="waitinglist/thanks.html"), name="waitinglist_thanks"),
