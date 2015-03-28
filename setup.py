731a8b09c23a06ad83d70d458bf548a87de3d77c
Patrick Altman <paltman@gmail.com>
2015-03-28 00:21:47 -0500

    Fix packaging

--- setup.py
+++ setup.py
@@ -2,20 +2,25 @@ from setuptools import setup, find_packages
 
 
 setup(
-    name = "django-waitinglist",
-    version = "1.4.4",
-    author = "Brian Rosner",
-    author_email = "brosner@gmail.com",
-    description = "a Django waiting list app for running a private beta with cohorts support",
-    long_description = open("README.rst").read(),
-    license = "MIT",
-    url = "http://github.com/pinax/django-waitinglist",
-    packages = find_packages(),
-    package_data = {"waitinglist": ["templates/*/*/*"]},
-    install_requires = [
+    name="django-waitinglist",
+    version="1.4.4",
+    author="Brian Rosner",
+    author_email="brosner@gmail.com",
+    description="a Django waiting list app for running a private beta with cohorts support",
+    long_description=open("README.rst").read(),
+    license="MIT",
+    url="http://github.com/pinax/django-waitinglist",
+    packages=find_packages(),
+    package_data={"waitinglist": [
+        "templates/waitinglist/email/initial_contact.txt",
+        "templates/waitinglist/email/initial_contact_subject.txt",
+        "templates/waitinglist/_list_signup.html",
+        "templates/waitinglist/_success.html"
+    ]},
+    install_requires=[
         "django-appconf==0.5",
     ],
-    classifiers = [
+    classifiers=[
         "Development Status :: 4 - Beta",
         "Environment :: Web Environment",
         "Intended Audience :: Developers",
